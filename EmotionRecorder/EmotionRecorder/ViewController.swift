8214c43a535b3b2809b6682c670dfeed0fd2dfb2
Cody <codyli332@gmail.com>
2016-05-26 10:01:31 -0400

    Very basic front camera preview working

--- EmotionRecorder/EmotionRecorder/ViewController.swift
+++ EmotionRecorder/EmotionRecorder/ViewController.swift
@@ -10,10 +10,59 @@ import UIKit
 import AVKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var playerViewController = AVPlayerViewController()
     var playerView = AVPlayer()
+    
+    var captureSession : AVCaptureSession?
+    var stillImageOutput : AVCaptureStillImageOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    @IBOutlet weak var cameraView: UIView!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPreset1280x720
+        
+        let videoDevices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        var captureDevice : AVCaptureDevice?
+        
+        for device in videoDevices{
+            let device = device as! AVCaptureDevice
+            if device.position == AVCaptureDevicePosition.Front{
+                captureDevice = device
+                break
+            }
+        }
+        
+        var error : NSError?
+        var input : AVCaptureDeviceInput!
+        do {
+            input = try AVCaptureDeviceInput(device : captureDevice)
+        }
+        catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        if (error == nil && captureSession?.canAddInput(input) != nil){
+            captureSession?.addInput(input)
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            
+            if(captureSession?.canAddOutput(stillImageOutput) != nil){
+                captureSession?.addOutput(stillImageOutput)
+                previewLayer = AVCaptureVideoPreviewLayer(session : captureSession)
+                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession?.startRunning()
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +70,13 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidLoad()
+        previewLayer?.frame = cameraView.bounds
     }
     
+    //Properties
     @IBOutlet weak var watchVideoButton: UIButton!
+    @IBOutlet weak var recordVideoButton: UIButton!
+    //
     
     @IBAction func playVideo(sender: AnyObject) {
         var fileURL = NSURL(fileURLWithPath: "/Users/codyli/Documents/Summer 2016 Internship/EmotionRecorder/EmotionRecorder/EmotionRecorder/Assets.xcassets/video.dataset/video.mp4")
